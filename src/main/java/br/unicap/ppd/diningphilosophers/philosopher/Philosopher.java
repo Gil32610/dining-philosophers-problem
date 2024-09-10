16e8c86fe0d501e509b1c3997a6d0f636dd7c478
gil.00000032610@unicap.br <gil.00000032610@unicap.br>
2024-09-10 14:33:12 -0300

    defining the tasks and resources

--- src/main/java/br/unicap/ppd/diningphilosophers/philosopher/Philosopher.java
+++ src/main/java/br/unicap/ppd/diningphilosophers/philosopher/Philosopher.java
@@ -1,6 +1,13 @@
 package main.java.br.unicap.ppd.diningphilosophers.philosopher;
 
+import main.java.br.unicap.ppd.diningphilosophers.fork.Fork;
+import main.java.br.unicap.ppd.diningphilosophers.spaghetti.Spaghetti;
+
+import java.util.List;
+
 public class Philosopher implements Runnable{
+    private List<Fork> availableForks;
+    private Spaghetti spaghetti;
     @Override
     public void run() {
         //TODO
